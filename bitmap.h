2d8beb42642a0f7b69e5c62916e24ae4ed71a871
Gabriel Raulet <gabe.h.raulet@gmail.com>
2022-09-27 11:52:13 -0700

    unnecessary

--- bitmap.h
+++ bitmap.h
@@ -3,11 +3,7 @@
 #include <stdint.h>
 #include "array.h"
 
-typedef struct
-{
-    uint64_t *v;
-    long n;
-} bitmap;
+typedef struct { uint64_t *v; long n; } bitmap;
 
 /**
  * @macro bitmap_items
